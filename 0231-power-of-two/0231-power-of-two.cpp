c2fd64c7edbdca03a244ce3b9fbec018696638ca
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-08-22 11:12:40 +0530

    Time: 0 ms (100%), Space: 6.2 MB (15.18%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+     for(int i=0;i<=30;i++){
+        int x =pow(2,i);
+
+
+          if(x==n){
+            return true ;
+            }
+                         }
+            return false;
+
+}
+};
