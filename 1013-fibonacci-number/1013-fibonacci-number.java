c8157b2ffb5420899124d0203a0d5650ed3f5324
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-08-24 17:34:51 +0530

    Time: 0 ms (100%), Space: 39.8 MB (8.07%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,17 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1)
-            return n;
-            return fib(n-1)+fib(n-2);
-        
+        // if(n<=1)
+        //     return n;
+        //     return fib(n-1)+fib(n-2);
+         int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
         
         }
         
