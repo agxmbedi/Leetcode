d92ee778995b41accda87f2a88a92afe021628c6
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-08-25 14:02:20 +0530

    Time: 0 ms (100%), Space: 44.3 MB (42.9%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,31 +1,27 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
          int n=nums.length;
-        //     int z=nums[n-1];
-        //     int sum=0;
-        //     int sum1=0;
-        //     for(int i=0;i<n;i++){
-        //          sum=sum+nums[i];
+         int sum=(n*(n+1))/2;
+         int s=0;
+         int miss;
+         for(int i=0;i<n;i++){
+             s+=nums[i];
+         }
+         miss=sum-s;
+        return miss;
 
-        //     }
-        //     for(int j=z;j>=0;j--){
-        //         sum1=sum1+j;
-
-        //     }
-        //     return sum1-sum;
-
-            int a[]=new int[n+1];
-            for(int i=0;i<=n;i++){
-                a[i]=i;
-            }
-            for(int j=0;j<n;j++){
-                if(a[j]!=nums[j])
-                return a[j];
-            }
+//             int a[]=new int[n+1];
+//             for(int i=0;i<=n;i++){
+//                 a[i]=i;
+//             }
+//             for(int j=0;j<n;j++){
+//                 if(a[j]!=nums[j])
+//                 return a[j];
+//             }
 
         
 
 
-return a[n];}
+// return a[n];}
+    }
 }
