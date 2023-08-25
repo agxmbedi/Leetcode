f3d45a13e2e8d80bbfc13396bc6402989dabfc51
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-08-25 15:40:52 +0530

    Time: 17 ms (41.17%), Space: 56.8 MB (37.74%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,44 +1,14 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // HashSet<Integer> set = new HashSet<>();
-        // for(int num : nums) {
-        //     if(!set.add(num)) {
-        //         return num;
-        //     }
-        // }
-        // return -1;
-    //     int count[] = new int [nums.length];
-    // for(int i = 0; i<nums.length; i++){
-    //     count[nums[i]]++;
-    //     if(count[nums[i]] > 1){
-    //         return nums[i];
-    //     } 
-    // }
-    // return nums.length;
-//      Arrays.sort(nums);
-//     for(int i = 0; i < nums.length - 1; i++){
-//         if(nums[i] == nums[i + 1]){
-//             return nums[i];
-//         }
-//     }
-//     return nums.length;
-// }
- int low = 0,high = nums.length - 1,cnt;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            cnt = 0;
-            for(int num:nums){
-                if(num <= mid){
-                    cnt++;
-                }
-            }
-            if(cnt <= mid){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(!set.add(num)) {
+                return num;
             }
         }
-    return low; }
+        return -1;
+    }
+
 }
     
         
