f2cae7e0edcafe69943d0980df802800d41c2ff7
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-08-30 21:47:21 +0530

    Time: 0 ms (100%), Space: 43.8 MB (10.89%) - LeetHub

---
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.java
@@ -0,0 +1,46 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findPeak(MountainArray mountainArr){
+        int s =0; 
+        int e = mountainArr.length() -1;
+        while(s<e){
+            int mid = s + (e-s)/2;
+            if(mountainArr.get(mid)>mountainArr.get(mid+1)) e = mid;
+            else s=mid+1;
+        }
+        return e;
+    }
+    public int BS(MountainArray mountainArr, int start, int end, int target, boolean left){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(left){
+                if(mountainArr.get(mid)> target) end = mid-1;
+                else if(mountainArr.get(mid)<target) start = mid+1;
+                else return mid;
+            }
+            else{
+                if(mountainArr.get(mid)> target) start = mid+1;
+            else if(mountainArr.get(mid)<target) end = mid-1;
+            else return mid;
+            }
+            
+        }
+        return -1;
+    }
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak = findPeak(mountainArr);
+        int ans = BS(mountainArr, 0, peak, target, true);
+        if(ans == -1){
+            ans = BS(mountainArr, peak+1, mountainArr.length() -1, target, false);
+        }
+        return ans;
+    }
+}
