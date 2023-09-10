fefd2056da22f82c59f619f50847bf1320fb05bb
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-09-10 11:43:32 +0530

    Time: 0 ms (100%), Space: 44 MB (25.81%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,20 @@
+ class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n]; // Corrected the length of the result array
+        int j = 0;
+        
+        for (int i = 0; i < n; i++) {
+            result[j] = nums[i];
+            j += 2;
+        }
+        
+        j = 1; // Reset j to 1 for the second half
+        
+        for (int i = n; i < 2 * n; i++) {
+            result[j] = nums[i];
+            j += 2;
+        }
+        
+        return result;
+    }
+}
