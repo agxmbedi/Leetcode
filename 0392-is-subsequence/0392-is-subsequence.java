d8d6885cfbe2df1a1da35c26f14403aa8db6816b
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-09-14 22:23:49 +0530

    Time: 1 ms (89.97%), Space: 40.1 MB (96.15%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,21 +1,35 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//       int len1=s.length();
+//       int len2=t.length();
+//       int x=0;
+//       if(len1==0){
+//         return true;
+//       }
+//       if(len1>len2){
+//         return false;
+//       }
+//       for(int i=0;i<len2;i++){
+//         if(s.charAt(x)==t.charAt(i)){
+//           if(x==len1-1){
+//             return true;
+//           }
+//           x++;
+//         }
+//       }
+//  return false;   }
+// }
 class Solution {
     public boolean isSubsequence(String s, String t) {
-      int len1=s.length();
-      int len2=t.length();
-      int x=0;
-      if(len1==0){
-        return true;
-      }
-      if(len1>len2){
-        return false;
-      }
-      for(int i=0;i<len2;i++){
-        if(s.charAt(x)==t.charAt(i)){
-          if(x==len1-1){
-            return true;
-          }
-          x++;
+        int sLen = s.length();
+        int tLen = t.length();
+        int j = 0;
+        
+        for (int i = 0; i < tLen && j < sLen; i++) {
+            if (s.charAt(j) == t.charAt(i)) 
+            j++;
         }
-      }
- return false;   }
+
+        return j == sLen; 
+    }
 }
