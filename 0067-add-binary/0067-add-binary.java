bd1384a67b9081f6cf424df5ad69e5d9998ae5ca
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-09-18 03:50:46 +0530

    Time: 1 ms (100%), Space: 40.9 MB (86.97%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,22 +1,21 @@
-class Solution {
-    public String addBinary(String a, String b) {
-        StringBuilder result = new StringBuilder();
-        int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+class Solution 
+{
+  public String addBinary(String a, String b) 
+  {
+    StringBuilder sb = new StringBuilder();
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
 
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int digitA = i >= 0 ? a.charAt(i) - '0' : 0;
-            int digitB = j >= 0 ? b.charAt(j) - '0' : 0;
-            int currentSum = digitA + digitB + carry;
-
-            result.insert(0, currentSum % 2);
-            carry = currentSum / 2;
-
-            i--;
-            j--;
-        }
-
-        return result.toString();
+    while (i >= 0 || j >= 0 || carry == 1) 
+    {
+      if(i >= 0)
+        carry += a.charAt(i--) - '0';
+      if(j >= 0)
+        carry += b.charAt(j--) - '0';
+      sb.append(carry % 2);
+      carry /= 2;
     }
+    return sb.reverse().toString();
+  }
 }
