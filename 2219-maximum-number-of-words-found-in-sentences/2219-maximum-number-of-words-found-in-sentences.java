ef63874aa61ea740e78b93d6d6db98f699021f17
agxmbedi <111156687+agxmbedi@users.noreply.github.com>
2023-09-18 22:55:37 +0530

    Time: 3 ms (90.9%), Space: 42.7 MB (63.9%) - LeetHub

---
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+      int max=0;
+      for(String x : sentences ){
+          int currlen=x.split(" ").length;
+          if(max<currlen){
+              max=currlen;
+          }
+      }
+        return max;}
+}
